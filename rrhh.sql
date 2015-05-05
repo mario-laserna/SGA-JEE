f4bcce7327b4d3c149c7c4a71db0054a9ae2f84e
mario <laserna.mario@gmail.com>
2015-05-04 21:43:09 -0500

    sql actualizado

--- rrhh.sql
+++ rrhh.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50141
 File Encoding         : 65001
 
-Date: 2015-04-14 15:55:04
+Date: 2015-05-04 21:42:47
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -34,5 +34,26 @@ CREATE TABLE `persona` (
 -- Records of persona
 -- ----------------------------
 INSERT INTO `persona` VALUES ('1', 'mario', 'laserna', null, 'laserna.mario@gmail.com', null);
-INSERT INTO `persona` VALUES ('2', 'aa', 'bb', 'cc', 'aa@bb.com', '123456');
+INSERT INTO `persona` VALUES ('2', 'Juan', 'Perez', '', 'jperez@mail.com', '123456');
 INSERT INTO `persona` VALUES ('3', 'aa', 'bb', 'cc', 'aa2@bb.com', '123456');
+
+-- ----------------------------
+-- Table structure for usuario
+-- ----------------------------
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `id_persona` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `username` (`username`),
+  KEY `fk_usuario_persona` (`id_persona`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of usuario
+-- ----------------------------
+INSERT INTO `usuario` VALUES ('1', 'jperez', '123', '2');
+INSERT INTO `usuario` VALUES ('2', 'usuario', 'contraseña', null);
+INSERT INTO `usuario` VALUES ('3', 'usuario2', 'contraseña', null);
